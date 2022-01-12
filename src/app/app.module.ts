cca6793cd12fffc8e62990c629094a41f39c8411
nikulin-anton <nikulin-dev@yandex.ru>
2022-01-12 10:48:17 +0500

    Added Taiga UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  TuiDialogModule,
+  TuiNotificationsModule,
+  TuiRootModule,
+  TUI_SANITIZER,
+} from '@taiga-ui/core';
+import { NgDompurifySanitizer } from '@tinkoff/ng-dompurify';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderModule } from './components/header/header.module';
 import { APP_NAME } from './const/app-name';
 import { APP_NAME_TOKEN } from './tokens/app-name.token';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule, HeaderModule],
-  providers: [{ provide: APP_NAME_TOKEN, useValue: APP_NAME }],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    HeaderModule,
+    TuiRootModule,
+    BrowserAnimationsModule,
+    TuiDialogModule,
+    TuiNotificationsModule,
+  ],
+  providers: [
+    { provide: APP_NAME_TOKEN, useValue: APP_NAME },
+    { provide: TUI_SANITIZER, useClass: NgDompurifySanitizer },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/components/header/header.module.ts
+++ src/app/components/header/header.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { TuiTabsModule } from '@taiga-ui/kit';
 import { HeaderComponent } from './header.component';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, TuiTabsModule],
   exports: [HeaderComponent],
 })
 export class HeaderModule {}
